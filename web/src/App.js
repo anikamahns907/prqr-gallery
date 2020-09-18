99cb596e99b7771bd87f74e4f367a60bc01bab8b
anikamahns907 <64509221+anikamahns907@users.noreply.github.com>
2020-09-17 23:57:09 -0400

    I like mountains and I also enjoy the hue of blue

---
+++ web/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './two-mountains.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>coming soon!</code>
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=g8sX6wZHhD0&ab_channel=MacMiller"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          good tunes
+        </a> */}
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/g8sX6wZHhD0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
