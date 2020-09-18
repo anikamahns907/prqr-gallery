86bf1225b39a9d6eebf06d71041ed136a9196f32
anikamahns907 <64509221+anikamahns907@users.noreply.github.com>
2020-09-18 00:03:43 -0400

    iframe

--- web/src/App.js
+++ web/src/App.js
@@ -18,7 +18,7 @@ function App() {
         >
           good tunes
         </a> */}
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/g8sX6wZHhD0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" title = "mac" src="https://www.youtube.com/embed/g8sX6wZHhD0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </header>
     </div>
   );
